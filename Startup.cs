0dbe597d3d753d794e471fc5456c79e0d63b510f
Dino Petrovic <Dino Petrovic>
2016-11-17 13:19:00 +0100

    Adding project to GitHub

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LeHömePage.Startup))]
+namespace LeHömePage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LeHömePage.MvcApplication" Language="C#" %>
